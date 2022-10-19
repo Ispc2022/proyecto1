b3e44887c371f8be19a57f6d29fc776701161ccf
Silvia1967 <106410716+Silvia1967@users.noreply.github.com>
2022-10-18 23:38:29 -0300

    Add files via upload

---
+++ MVC/CONTROLADORES/4.informes.py
@@ -0,0 +1,9 @@
+from os import supports_bytes_environ
+
+
+el administrador puede generar informes sobre caja movientos y empleados.
+
+
+
+archivos usados informe_ventas.html, informecaja.html, informeusuarios.html
+
---
+++ MVC/CONTROLADORES/menuadmin.py
@@ -0,0 +1,5 @@
+generamos un menu llamado ajustes que solo tiene acceso el administrador
+para dar alta y bajas de usuarios, actualizar tarifas y ver informes detallados
+
+
+archivos usados: AJUSTES.HTML, ALTASYBAJASUSUARIOS.HTML.
