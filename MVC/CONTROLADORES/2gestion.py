69ab44bed72ee7af025adbd98c22451c5542aeab
Priscilat93 <106410624+Priscilat93@users.noreply.github.com>
2022-10-18 23:35:22 -0300

    Add files via upload

---
+++ MVC/CONTROLADORES/2gestion.py
@@ -0,0 +1,15 @@
+el usuario da ingreso a la base de datos los datos del cliente
+se genera un horario de entrada y un ticket
+el usuario da de baja la estadia y genera un numero de referencia para la caja
+
+
+
+
+
+
+
+
+
+
+
+archivo usado: ventas.html
---
+++ MVC/CONTROLADORES/3caja.py
@@ -0,0 +1,5 @@
+el usuario levanta los datos de la base de datos con el numero de referencia
+selecciona metodo de pago y confirma el monto a la caja.
+
+
+archivo usado: caja_diaria.html
