15d0d39be113484769ac3f51a2efcd23d18f15af
Emiliano Guazzetti <106410278+Ispc2022@users.noreply.github.com>
2022-10-18 23:29:14 -0300

    Add files via upload

---
+++ MVC/CONTROLADORES/1login.py
@@ -0,0 +1,11 @@
+from sqlite3 import DateFromTicks
+from xml.dom import ValidationErr
+
+
+el usuario ingresa usuario y contraseña
+los datos son enviados a la base de Datos 
+se Validan : si son correctos tiene acceso ; si no son corretos se genera un alert de error.
+
+
+
+archivo usado: login.html
